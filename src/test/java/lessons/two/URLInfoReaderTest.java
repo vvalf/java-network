ad6aa19eb04310c41de6d711a24c919e1de3e64c
fedotovskikh-vv <vvfedotovskikh@sberbank.ru>
2022-11-03 16:59:55 +0300

    lesson2 get URL Info and tests

---
+++ src/test/java/lessons/two/URLInfoReaderTest.java
@@ -0,0 +1,20 @@
+package lessons.two;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class URLInfoReaderTest {
+
+    @Test
+    public void getURLInfoToConsole() throws IOException {
+        URLInfoReader urlInfoReader = new URLInfoReader();
+        urlInfoReader.getURLInfoToConsole("https://ya.ru/");
+        urlInfoReader.getURLInfoToConsole("https://wrongURL.wrongURL/");
+
+        assertThrows(IOException.class, () -> urlInfoReader.getURLInfoToConsole("wrongURL.wrongURL"));
+
+    }
+}
