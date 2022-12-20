456977c47d0e2e04041acea660bf9ad8714e5b2b
fedotovskikh-vv <vvfedotovskikh@sberbank.ru>
2022-12-20 12:38:36 +0300

    FinalProject create server and client

---
+++ finalProjectServer/src/main/java/finalProject/FinalProjectServer.java
@@ -0,0 +1,26 @@
+package finalProject;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class FinalProjectServer {
+    public static void main(String[] args) throws InterruptedException {
+        var eventLoopGroup = new NioEventLoopGroup(1);
+        var server = new ServerBootstrap()
+                .group(eventLoopGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ServerChannelInitializer());
+        try {
+            var channelFuture = server.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+}
